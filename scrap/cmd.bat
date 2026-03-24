11835cd5130f7d456e8686996df1aea456cfd83a
rapip <rafifgenadratama28@gmail.com>
2026-03-24 13:50:51 +0700

    prepare for hugo revamp

--- scrap/cmd.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-"C:\Users\rafif\OneDrive - Politeknik Negeri Bandung\sem 2\Semester 2\Proyek 1\Pertemuan 8\Source Code\kupip.github.io\venv\Scripts\python.exe" "C:\Users\rafif\OneDrive - Politeknik Negeri Bandung\sem 2\Semester 2\Proyek 1\Pertemuan 8\Source Code\kupip.github.io\scrap\scrapping.py"
-pause
